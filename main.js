3e7c483e05accc082a3ef6e72725b94ef2176eb8
Hamdi Mohamed <31364936+x0911@users.noreply.github.com>
2021-08-24 22:09:08 +0200

    fix-symbols

--- main.js
+++ main.js
@@ -1,75 +1,106 @@
 // variable
-      const operation = document.querySelector('.operation');
-      const result = document.querySelector('.result');
-      const clear = document.querySelector('.clear');
-      const operators = document.querySelectorAll('.operator');
-      const nums = document.querySelectorAll('.num');
-      const evaluate = document.querySelector('.eval');
-      const del = document.querySelector('.del');
-      const percent = document.querySelector('.percent');
-      const realoperators= ['+','-','*','/','.'];
-      console.log(realoperators);
+const operation = document.querySelector(".operation");
+const result = document.querySelector(".result");
+const clear = document.querySelector(".clear");
+const operators = document.querySelectorAll(".operator");
+const nums = document.querySelectorAll(".num");
+const evaluate = document.querySelector(".eval");
+const del = document.querySelector(".del");
+const percent = document.querySelector(".percent");
+const realoperators = ["+", "-", "×", "÷", "."];
+console.log(realoperators);
 
-      // clear btn
-      clear.addEventListener('click', function () {
-        operation.value = '';
-        result.textContent = '';
-      });
+const removeTags = (text) => {
+  return text.replace(/(<([^>]+)>)/gi, "");
+};
 
-      // del btn
-      del.addEventListener('click',function(){
-        operation.value = operation.value.slice(0, -1);
-        result.textContent =  eval(operation.value);
-      });
+const fixSymbols = (text) => {
+  const newText = removeTags(text);
+  return newText.replace(/×/g, "*").replace(/÷/g, "/");
+};
 
-      // avoid repeating operators and dot
-      for(const num of nums){
-        num.addEventListener('click', function () {
-          //  clear result after eval 
-          if (operation.value === "") result.textContent = '';
-          operation.value += num.getAttribute('value');
-          result.textContent =  eval(operation.value);
-          result.style.color = 'rgb(92, 92, 92,0.8)';
-        })
-      }; 
-      for(const operator of operators){
-        operator.addEventListener('click', function () {
-          // clear result after eval and pass result
-          if (operation.value === "") {
-            operation.value = result.textContent;
-            result.textContent = '';
-          }
-          // repeating 
-          if (operation.value[operation.value.length - 1] === operator.getAttribute('value')) {
-          operation.value += '';
-          } 
-          // move from operator to other
-          else if( 
-            realoperators.includes(operation.value[operation.value.length - 1]) &&
-            operation.value[operation.value.length - 1] !== operator.getAttribute('value')
-          ){
-            console.log('i am here');
-            operation.value = operation.value.slice(0, -1);
-            operation.value += operator.getAttribute('value');
-            // operation.value[operation.value.length - 1] = operator.getAttribute('value');
-          }
-          else {operation.value += operator.getAttribute('value');}
-      })};
+const setvalue = (text) => {
+  let v = "";
+  if (text) {
+    v = eval(fixSymbols(text));
+  }
+  result.innerHTML = v;
+};
 
-      // percent btn
-      percent.addEventListener('click', function () {
-        operation.value = `(${operation.value})/100`;
-        result.textContent =  eval(operation.value);
-      });
+const setOperation = (text) => {
+  operation.innerHTML = text;
+};
 
-      // eval btn
-      evaluate.addEventListener('click', function() {
-        if (operation.value === '') {
-        result.textContent = 'not valid';
-      }  else {
-        result.textContent = eval(operation.value);
-        // result.textContent ? result.textContent : eval(operation.value);
-        operation.value = '';
-        result.style.color = '#000';
-        result.style.fontWeight = '500';
-      }});
+// clear btn
+clear.addEventListener("click", function () {
+  setOperation("");
+  setvalue("");
+});
+
+// del btn
+del.addEventListener("click", function () {
+  const v = operation.innerHTML;
+  setOperation(v.slice(0, -1));
+  setvalue(v);
+});
+
+// avoid repeating operators and dot
+for (const num of nums) {
+  num.addEventListener("click", function () {
+    //  clear result after eval
+    if (operation.innerHTML.trim() === "") setvalue("");
+    setOperation(operation.innerHTML + num.getAttribute("value"));
+    setvalue(operation.innerHTML);
+    result.style.color = "rgb(92, 92, 92,0.8)";
+  });
+}
+for (const operator of operators) {
+  operator.addEventListener("click", function () {
+    // clear result after eval and pass result
+    if (operation.innerHTML.trim() === "") {
+      setOperation(result.innerHTML);
+      setvalue("");
+    }
+    // repeating
+    if (
+      operation.innerHTML.trim()[operation.innerHTML.trim().length - 1] ===
+      operator.getAttribute("value")
+    ) {
+      setOperation(operation.innerHTML + "");
+    }
+    // move from operator to other
+    else if (
+      realoperators.includes(
+        operation.innerHTML.trim()[operation.innerHTML.trim().length - 1]
+      ) &&
+      operation.innerHTML.trim()[operation.innerHTML.trim().length - 1] !==
+        operator.getAttribute("value")
+    ) {
+      console.log("i am here");
+      setOperation(operation.innerHTML.trim().slice(0, -1));
+      setOperation(operation.innerHTML.trim() + operator.getAttribute("value"));
+    } else {
+      setOperation(operation.innerHTML.trim() + operator.getAttribute("value"));
+    }
+  });
+}
+
+// percent btn
+percent.addEventListener("click", function () {
+  const r = result.innerHTML;
+  const v = `( <sup>${r}</sup>/<sub>100</sub> )`;
+  setOperation(v);
+  setvalue(operation.innerHTML);
+});
+
+// eval btn
+evaluate.addEventListener("click", function () {
+  if (operation.innerHTML.trim() === "") {
+    setvalue("not valid", true);
+  } else {
+    setvalue(operation.innerHTML);
+    setOperation("");
+    result.style.color = "#000";
+    result.style.fontWeight = "500";
+  }
+});
